0b0b291bd798be57439ad8da61245713d9374adb
Dariush Tasdighi <dariusht@gmail.com>
2024-10-01 01:02:30 +0330

    Version 2.5

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # **************************************************
 # دوربین امنیتی
-# Version 2.4
+# Version 2.5
+# نکته مهم و جالب
+# می‌کنیم نیز، کار می‌کند Lock این برنامه، حتی زمانی که سیستم را
 # **************************************************
 import os
 import time
@@ -10,115 +12,136 @@ from colorama import Fore
 from datetime import datetime
 
 # Initial Settings
-webcam_index = 0
-sensitivity = 50
-path = "C:\\Captures"
-display_brand = True
-max_resolution = True
-display_frames = True
-display_difference = True
-display_notification = True
+WEBCAM_INDEX = 0
+SENSITIVITY = 100
+PATH = "C:\\Captures"
+DISPLAY_BRAND = True
+MAX_RESOLUTION = True
+DISPLAY_FRAMES = True
+DISPLAY_DIFFERENCE = True
+INITIAL_DELAY_SECONDS = 10
+DISPLAY_NOTIFICATION = True
 
-# webcam_index = 0
-# sensitivity = 50
-# path = "C:\\Captures"
-# display_brand = False
-# max_resolution = False
-# display_frames = False
-# display_difference = False
-# display_notification = False
+# WEBCAM_INDEX = 0
+# SENSITIVITY = 100
+# PATH = "C:\\Captures"
+# DISPLAY_BRAND = True
+# MAX_RESOLUTION = True
+# DISPLAY_FRAMES = False
+# DISPLAY_DIFFERENCE = False
+# INITIAL_DELAY_SECONDS = 10
+# DISPLAY_NOTIFICATION = False
 
 
-def print_current_webcam_settings():
+def print_current_webcam_settings() -> None:
     fps = webcam.get(propId=cv.CAP_PROP_FPS)
     width = webcam.get(propId=cv.CAP_PROP_FRAME_WIDTH)
     height = webcam.get(propId=cv.CAP_PROP_FRAME_HEIGHT)
 
-    print(f"Current Webcam FPS: {fps}")
-    print(f"Current Webcam Resolution: {width} * {height}")
+    print(f"Current webcam FPS: {fps}")
+    print(f"Current webcam Resolution: {width} * {height}")
 
 
-def change_webcam_resolution(width: int, height: int):
+def change_webcam_resolution(width: int, height: int) -> None:
     webcam.set(propId=cv.CAP_PROP_FRAME_WIDTH, value=width)
     webcam.set(propId=cv.CAP_PROP_FRAME_HEIGHT, value=height)
 
 
-def get_difference(image1, image2):
-    image1_mean = np.mean(a=image1)
-    image2_mean = np.mean(a=image2)
-    difference_frames = np.abs(image1_mean - image2_mean) * 100
+def get_difference(frame1: cv.Mat, frame2: cv.Mat) -> int:
+    frame1_mean = np.mean(a=frame1)
+    frame2_mean = np.mean(a=frame2)
+    difference = np.abs(frame1_mean - frame2_mean) * 100
 
-    return difference_frames
+    return difference
+
+
+def write_text_on_frame(frame: cv.Mat, text: str) -> None:
+    frame_height = frame.shape[0]
+
+    cv.putText(
+        img=frame,
+        text=text,
+        org=(10, frame_height - 15),
+        fontFace=cv.FONT_HERSHEY_SIMPLEX,
+        fontScale=1.0,
+        color=(0, 0, 255),
+        thickness=2,
+        lineType=cv.LINE_4,
+    )
+
+
+def check_motion_detection(
+    new_frame: cv.Mat,
+    new_grayscale_frame: cv.Mat,
+    last_grayscale_frame: cv.Mat,
+) -> None:
+    difference = get_difference(frame1=new_grayscale_frame, frame2=last_grayscale_frame)
+
+    if DISPLAY_DIFFERENCE:
+        print(difference)
+
+    if difference < SENSITIVITY:
+        return
+
+    now = datetime.now()
+    formated_now = now.strftime(format="%Y_%m_%d_%H_%M_%S")
+
+    if DISPLAY_NOTIFICATION:
+        print(f"{Fore.RED}{formated_now}: Motion Detected!{Fore.RESET}")
+
+    filename = f"Capture_{formated_now}.png"
+    pathname = f"{PATH}\\{filename}"
+
+    if DISPLAY_BRAND:
+        brand = f"Dariush Tasdighi - 09121087461 - @IranianExperts - {formated_now}"
+        write_text_on_frame(frame=new_frame, text=brand)
+
+    cv.imwrite(filename=pathname, img=new_frame)
 
 
 os.system(command="cls")
 
-webcam = cv.VideoCapture(index=webcam_index)
+webcam = cv.VideoCapture(index=WEBCAM_INDEX)
 if not webcam.isOpened():
     print("You do not have any webcam or it is not ready to use!")
     webcam.release()
     quit()
 
 print_current_webcam_settings()
-if max_resolution:
+if MAX_RESOLUTION:
     change_webcam_resolution(width=20_000, height=20_000)
     print_current_webcam_settings()
 
-last_frame = None
-
-time.sleep(9)
+last_grayscale_frame = None
+start_time = datetime.now()
 
 while True:
     time.sleep(1)
 
-    result, original_frame = webcam.read()
+    success, new_frame = webcam.read()
 
-    if result:
-        now = datetime.now()
-        formated_now = now.strftime(format="%Y_%m_%d_%H_%M_%S")
+    if success:
+        new_grayscale_frame = cv.cvtColor(src=new_frame, code=cv.COLOR_BGR2GRAY)
 
-        grayscale_frame = cv.cvtColor(src=original_frame, code=cv.COLOR_BGR2GRAY)
-
-        if display_frames:
-            cv.imshow(winname="Webcam", mat=original_frame)
-
-        if last_frame is None:
-            last_frame = grayscale_frame
+        if last_grayscale_frame is None:
+            last_grayscale_frame = new_grayscale_frame
             continue
 
-        difference = get_difference(image1=grayscale_frame, image2=last_frame)
+        if DISPLAY_FRAMES:
+            cv.imshow(winname="Webcam", mat=new_frame)
 
-        if display_difference:
-            print(difference)
+        difference_time = datetime.now() - start_time
+        if difference_time.total_seconds() < INITIAL_DELAY_SECONDS:
+            cv.waitKey(delay=1)
+            print("Initialization...")
+        else:
+            check_motion_detection(
+                new_frame=new_frame,
+                new_grayscale_frame=new_grayscale_frame,
+                last_grayscale_frame=last_grayscale_frame,
+            )
 
-        if difference > sensitivity:
-            if display_notification:
-                print(f"{Fore.RED}{formated_now}: Motion Detected!{Fore.RESET}")
-
-            filename = f"Capture_{formated_now}.png"
-            pathname = f"{path}\\{filename}"
-
-            if display_brand:
-                formated_now = now.strftime(format="%Y/%m/%d %H:%M:%S")
-
-                brand = (
-                    f"Dariush Tasdighi - 09121087461 - @IranianExperts - {formated_now}"
-                )
-
-                cv.putText(
-                    img=original_frame,
-                    text=brand,
-                    org=(10, original_frame.shape[0] - 15),
-                    fontFace=cv.FONT_HERSHEY_SIMPLEX,
-                    fontScale=1.0,
-                    color=(0, 0, 255),
-                    thickness=2,
-                    lineType=cv.LINE_4,
-                )
-
-            cv.imwrite(filename=pathname, img=original_frame)
-
-        last_frame = grayscale_frame
+            last_grayscale_frame = new_grayscale_frame
 
     key = cv.waitKey(delay=1)
     if key == ord("q") or key == ord("Q"):
@@ -126,4 +149,3 @@ while True:
 
 webcam.release()
 cv.destroyAllWindows()
-# **************************************************
