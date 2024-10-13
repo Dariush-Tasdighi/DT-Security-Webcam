8c4daf2a97ac6cd299b5ad4203c67b596c5e7b8d
Dariush Tasdighi <dariusht@gmail.com>
2024-10-13 11:32:21 +0330

    Version 2.9

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 # **************************************************
 # DT Webcam Security
-# Version 2.8
-# نکته مهم و جالب
-# می‌کنیم نیز، کار می‌کند Lock این برنامه، حتی زمانی که سیستم را
-# با اجرای چند نسخه از این برنامه، می‌توان از چند وبکم استفاده کرد
+# Version 2.9
 # **************************************************
 import os
 import time
@@ -15,57 +12,60 @@ from datetime import datetime
 
 
 def main() -> None:
-    os.system(command="cls")
+    try:
+        os.system(command="cls")
 
-    webcam = cv.VideoCapture(index=constants.WEBCAM_INDEX)
-    if not webcam.isOpened():
-        error = f"{Fore.RED}You do not have any webcam or it is not ready to use!{Fore.RESET}"
-        print(error)
-        webcam.release()
-        quit()
+        webcam = cv.VideoCapture(index=constants.WEBCAM_INDEX)
+        if not webcam.isOpened():
+            error = f"{Fore.RED}You do not have any webcam or it is not ready to use!{Fore.RESET}"
+            print(error)
+            webcam.release()
+            quit()
 
-    functions.print_current_webcam_settings(webcam=webcam)
-    if constants.MAX_RESOLUTION:
-        functions.change_webcam_resolution(webcam=webcam, width=1280, height=720)  # HD
         functions.print_current_webcam_settings(webcam=webcam)
+        if constants.MAX_RESOLUTION:
+            functions.change_webcam_resolution(webcam=webcam, width=1280, height=720)
+            functions.print_current_webcam_settings(webcam=webcam)
 
-    last_grayscale_frame = None
-    start_time = datetime.now()
+        last_grayscale_frame = None
+        start_time = datetime.now()
 
-    while True:
-        time.sleep(1)
+        while True:
+            time.sleep(1)
 
-        success, new_frame = webcam.read()
+            success, new_frame = webcam.read()
 
-        if success:
-            new_grayscale_frame = cv.cvtColor(src=new_frame, code=cv.COLOR_BGR2GRAY)
+            if success:
+                new_grayscale_frame = cv.cvtColor(src=new_frame, code=cv.COLOR_BGR2GRAY)
 
-            if last_grayscale_frame is None:
-                last_grayscale_frame = new_grayscale_frame
-                continue
+                if last_grayscale_frame is None:
+                    last_grayscale_frame = new_grayscale_frame
+                    continue
 
-            if constants.DISPLAY_FRAMES:
-                cv.imshow(winname="Webcam", mat=new_frame)
+                if constants.DISPLAY_FRAMES:
+                    cv.imshow(winname="Webcam", mat=new_frame)
 
-            difference_time = datetime.now() - start_time
-            if difference_time.total_seconds() < constants.INITIAL_DELAY:
-                cv.waitKey(delay=1)
-                print(f"{Fore.YELLOW}Initialization...{Fore.RESET}")
-            else:
-                functions.check_motion_detection(
-                    new_frame=new_frame,
-                    new_grayscale_frame=new_grayscale_frame,
-                    last_grayscale_frame=last_grayscale_frame,
-                )
+                difference_time = datetime.now() - start_time
+                if difference_time.total_seconds() < constants.INITIAL_DELAY:
+                    cv.waitKey(delay=1)
+                    print(f"{Fore.YELLOW}Initialization...{Fore.RESET}")
+                else:
+                    functions.check_motion_detection(
+                        new_frame=new_frame,
+                        new_grayscale_frame=new_grayscale_frame,
+                        last_grayscale_frame=last_grayscale_frame,
+                    )
 
-                last_grayscale_frame = new_grayscale_frame
+                    last_grayscale_frame = new_grayscale_frame
 
-        key = cv.waitKey(delay=1)
-        if key == ord("q") or key == ord("Q"):
-            break
-
-    webcam.release()
-    cv.destroyAllWindows()
+            key = cv.waitKey(delay=1)
+            if key == ord("q") or key == ord("Q"):
+                break
+    except KeyboardInterrupt:
+        pass
+    finally:
+        webcam.release()
+        cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
--- constants.py
+++ constants.py
@@ -1,14 +1,14 @@
-WEBCAM_INDEX: int = 0
+WEBCAM_INDEX: int = 0  # -1 [OR] 0 [OR] 1 ...
 SENSITIVITY: int = 120
 PATH: str = "C:\\Captures"
 MAX_RESOLUTION: bool = True  # HD: 1280x720
 
-DISPLAY_FRAMES: bool = True
 PUT_TEXT_ON_FRAME: bool = True
-DISPLAY_NOTIFICATION: bool = True
-DISPLAY_FRAMES_DIFFERENCE: bool = True
+DISPLAY_FRAMES: bool = True  # False
+DISPLAY_NOTIFICATION: bool = True  # False
+DISPLAY_FRAMES_DIFFERENCE: bool = True  # False
 
 INITIAL_DELAY: int = 10  # Second(s)
 CAPTURE_INTERVAL: int = 1  # Second(s)
 
-BRAND: str = "Dariush Tasdighi - @IranianExperts - 09121087461"
+BRAND: str = "Dariush Tasdighi - @IranianExperts - 09121087461"  # Your Company / Name
